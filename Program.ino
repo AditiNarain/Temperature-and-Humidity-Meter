ffe8431e432fef4f5775b96e1af3acf8ea3636f9
Aditi <aditinarain.60@gmail.com>
2021-01-26 11:37:22 +0530

    Arduino IDE program of the project

---
+++ Program.ino
@@ -0,0 +1,39 @@
+#include "DHT.h"
+#define DHTPIN 7     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11   // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// for DHT11, 
+//      VCC: 5V or 3V
+//      GND: GND
+//      DATA: 7
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHT11 test!");
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+
+
+
+  
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.println("%");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println("°C ");
+
+}
